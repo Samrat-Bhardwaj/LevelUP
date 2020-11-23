5789fdaa30ed5cb5faf0fac98bf1c2ff9fd4f9f4
SAM Bhardwaj <bsamrat47@gmail.com>
2020-11-24 00:32:58 +0530

    23/11/2020

--- Tree/babar.java
+++ Tree/babar.java
@@ -13,6 +13,87 @@ public class babar {
 	
 }
 
+public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+}
+
+// hourse robber-3 (ek ghar rob kro to uska next nhi kr skte) =======================================
+
+// noice approach =======
+
+/* naive ==============================================================
+
+    unordered_map<TreeNode*,int> t;
+    unordered_map<TreeNode*,int> f;
+    int getMax(TreeNode* root, bool fetch){
+        if(root==nullptr) return 0;
+        
+        int ans=-1;
+        if(fetch){
+            if(t.find(root)!=t.end())
+                return t[root];
+        } else {
+            if(f.find(root)!=f.end()){
+                return f[root];
+            }
+        }
+        
+        if(!fetch){
+            int temp=getMax(root->left,true)+getMax(root->right,true) + root->val;
+            temp=max(temp,getMax(root->right,false)+getMax(root->left,false));
+            ans=max(ans,temp);
+        } else {
+            ans=max(ans,getMax(root->right,false)+getMax(root->left,false));
+        }
+        if(fetch){
+            t[root]=ans;
+        } else {
+            f[root]=ans;
+        }
+        
+        return ans;
+    }
+    
+    int rob(TreeNode* root) {
+        int ans=getMax(root,true);
+        ans=max(ans,getMax(root,false));
+        
+        return ans;
+    }
+
+*/
+
+// now rather than calling function two times with true n false, we can just call it 1 time using array
+// of two size 
+
+public int[] getMax(TreeNode root){
+    if(root==null) return new int[]{0,0};
+   
+   int[] l=getMax(root.left);
+   int[] r=getMax(root.right);
+   
+   // ye vala robbing, iske bache nhi rob krenge;
+   int rob=root.val + l[1] + r[1];
+   
+   // not robbing ye vala, niche vala rob krna na krna marji hamari;
+   int notRob=Math.max(l[0],l[1]) + Math.max(r[0],r[1]);
+   
+   return new int[]{rob,notRob};
+}
+
+public int rob(TreeNode root) {
+   int ans[]=getMax(root);
+   return Math.max(ans[0],ans[1]);
+}
 // binary tree to DLL
 
 // https://practice.geeksforgeeks.org/problems/binary-tree-to-dll/1
